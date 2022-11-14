d8179fefc355ec476107218e1e1af380374792bf
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-14 11:02:39 +0100

    Jalon1-exo01

---
+++ jalon1/exo01/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head> 
+    <title>PHP donne l'heure</title> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+    <p>PHP donne l'heure : <?php echo date("d/m/Y H:i:s", time()+3600)?>.</p>
+</body>

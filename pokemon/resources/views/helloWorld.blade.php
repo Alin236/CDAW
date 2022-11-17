aa54a44ad0561831569612028e867eb2fd250378
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-17 09:31:52 +0100

    Hello world

---
+++ pokemon/resources/views/helloWorld.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head> 
+    <title>PHP donne l'heure</title> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body>
+    <p>Hello world from view</p>
+</body>

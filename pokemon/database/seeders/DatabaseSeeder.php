686ffbcd1fd112e4d8b65fd868976316f4370b21
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-18 10:24:00 +0100

    Pokemon seeder

--- pokemon/database/seeders/DatabaseSeeder.php
+++ pokemon/database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(EnergySeeder::class);
+        $this->call(PokemonSeeder::class);
     }
 }
---
+++ pokemon/database/seeders/PokemonSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PokemonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $pokemonJsonResults = json_decode(file_get_contents('https://pokeapi.co/api/v2/pokemon'))->results;
+        for($i=1; $i<=Count($pokemonJsonResults); $i++){
+            DB::table('pokemon')->insert(['id' => $i, 'energy_id'=> rand(1, 10), 'name' => $pokemonJsonResults[$i-1]->name, 'pv_max' => rand(20,30), 'level' => rand(1,10), 'path' => "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$i.png"]);
+        }
+    }
+}

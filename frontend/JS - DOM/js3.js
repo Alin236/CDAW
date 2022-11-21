dad4a11b2489a4f0c0a87d3231afb08a2949500b
Nilavan DEVA <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-21 16:34:18 +0100

    Ajout d'un commentaire avec le bon id

--- frontend/JS - DOM/js3.js
+++ frontend/JS - DOM/js3.js
@@ -10,12 +10,21 @@ function deleter(e)
     e.srcElement.parentElement.hidden = true;
 }
 
+let i = 4;
 document.getElementById("addNew").addEventListener("click", function(e){
-    alert(e.type +" on add !");
+    let html = '<div><h4>Moi</h4><p>Nouveau commentaire.</p><button class="modify">Modify Comment</button><button class="remove">Remove Comment</button></div>';
+    let users = document.getElementById("users");
+    users.innerHTML+=html;
+    initBouton();
+    users.lastChild.id = "user" + i;
+    i++;
 })
 
-let modifiers = document.getElementsByClassName("modify");
-Array.from(modifiers).forEach(m => m.addEventListener("click",modify));
+function initBouton(){
+    let modifiers = document.getElementsByClassName("modify");
+    Array.from(modifiers).forEach(m => m.addEventListener("click",modify));
 
-let remover = document.getElementsByClassName("remove");
-Array.from(remover).forEach(m => m.addEventListener("click",deleter));
+    let remover = document.getElementsByClassName("remove");
+    Array.from(remover).forEach(m => m.addEventListener("click",deleter));
+}
+initBouton();

808ca99d4583c6b3d8a06332d276ac0a0a95c90b
Nilavan DEVA <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-21 17:53:01 +0100

    Correction du script initialisant la datatable

--- frontend/exercice.php
+++ frontend/exercice.php
@@ -4,11 +4,8 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-
 <script>
     $(document).ready( function () {
-        $('#myTable').DataTable(
-            data: <?php $json->results ?>
-        );
+        $('#myTable').DataTable();
     });
 </script>

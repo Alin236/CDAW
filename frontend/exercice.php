e72e175ce101f97af24ea030c74c894488a5db4c
Nilavan DEVA <nilavan.deva@etu.imt-nord-europe.fr>
2022-11-21 17:51:42 +0100

    Correction importe css et datatable (brouillon)

--- frontend/exercice.php
+++ frontend/exercice.php
@@ -1,10 +1,10 @@
 <?php
     $json = json_decode(file_get_contents('https://pokeapi.co/api/v2/pokemon'));
 ?>
-<link rel="stylesheet" href="cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-<script src="cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<table id="myTable">
-</table>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+<script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+
 <script>
     $(document).ready( function () {
         $('#myTable').DataTable(

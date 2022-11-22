9d00b50d49de74e300d4062cc422a3ebdb2875f9
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-22 18:10:29 +0100

    Seeder maitrise amélioré

--- pokemon/database/seeders/MaitriseSeeder.php
+++ pokemon/database/seeders/MaitriseSeeder.php
@@ -4,9 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\{Trainer, Energy};
 
 class MaitriseSeeder extends Seeder
 {
+    private $energies;
+    private $trainer;
     /**
      * Run the database seeds.
      *
@@ -18,6 +21,19 @@ class MaitriseSeeder extends Seeder
         DB::table('maitrise')->insert(['id_trainer' => 1, 'id_energy' => 10]);
         DB::table('maitrise')->insert(['id_trainer' => 2, 'id_energy' => 18]);
         DB::table('maitrise')->insert(['id_trainer' => 2, 'id_energy' => 8]);
-        
+
+        $this->energies = Energy::all();
+        Trainer::all()
+            ->filter(function ($trainer) {
+                return $trainer->id != 1 && $trainer->id != 2;
+            })
+            ->each(function ($trainer){
+                $this->trainer = $trainer;
+                $number = rand(1,10);
+                $energiesMastered = $this->energies->random($number);
+                $energiesMastered->each(function ($energyMastered){
+                    DB::table('maitrise')->insert(['id_trainer' => $this->trainer->id, 'id_energy' => $energyMastered->id]);
+                });
+            });
     }
 }

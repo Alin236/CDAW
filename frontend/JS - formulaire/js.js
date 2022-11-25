fc1cc01d6f8664d4c26f850537d6bd1a55e8ca95
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-25 04:30:27 +0100

    Indiquer l'utilisateur du commentaire modifié et modifié le commentaire

--- frontend/JS - formulaire/js.js
+++ frontend/JS - formulaire/js.js
@@ -1,8 +1,10 @@
 "use strict";
 
+let id = '';
 function modify(e)
 {
     document.forms.myForm.elements[0].innerHTML = e.srcElement.parentElement.children[1].innerHTML;
+    id = e.srcElement.parentElement.id;
     document.forms.myForm.style.display = 'block';
     document.forms.myForm.elements[1].disabled = false;
 }
@@ -36,5 +38,12 @@ document.forms.myForm.addEventListener("submit", function(event){
     if(comment == ''){
         event.preventDefault();
         document.getElementById('message').innerHTML = "Message non valide";
+        return;
     }
+    document.getElementById('message').innerHTML = "";
+    let section = document.getElementById(id);
+    let pseudo = section.children[0].innerHTML;
+    alert('Commentaire de ' + pseudo + ' modifié');
+    section.children[1].innerHTML = comment;
+    event.preventDefault();
 });

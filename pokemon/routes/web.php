5b26adf454c18984b42d9ed2fff6a3fc9dd35f9b
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-11-25 15:05:16 +0100

    php artisan breeze:install

--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,50 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/')->group(function () {
-    Route::get('/pokedex', 'App\Http\controllers\PokemonController@index');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::prefix('/decouverteDeLaravel')->group(function () {
-    Route::get('/', function () {
-        return 'Hello world from return';
-    });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::get('/hello', function () {
-        echo 'Hello world from echo';
-    });
-
-    Route::get('/listePokemons', function () {
-        return 'Liste des pokémons';
-    });
-
-    Route::get('/mauvaiseFacon', function () {
-        return
-    '
-    <!doctype html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Mauvaise façon</title>
-    </head>
-    <body>
-        <p>Le fichier risque d\'être longggggg</p>
-    </body>
-    </html>
-    ';
-    });
-
-    Route::get('/listePokemonsView', function () {
-        return view('listePokemons');
-    });
-
-    Route::get('/listePokemonsController/{param?}', 'App\Http\controllers\listePokemonsController@getListePokemons');
-
-    Route::get('/{prenom}/{nom}', function ($prenom, $nom) {
-        return "Hello $prenom $nom";
-    });
-
-    Route::get('/{title}', function ($title) {
-        return "Film : $title";
-    })->where('title', '[a-zA-Z ]+');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';

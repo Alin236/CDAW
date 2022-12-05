0c075142d99fdf250e5bfd1fb4b201d2c76d505f
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-05 09:36:40 +0100

    Déplacement de code

--- pokemon/resources/views/template.blade.php
+++ pokemon/resources/views/template.blade.php
@@ -11,8 +11,7 @@
         <section>
             @yield('content')
         </section>
-
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        @yield('javascript')
     </body>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    @yield('javascript')
 </html>

cdf60fc93c60e65d9173e6565464199bd5b8028f
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-05 09:49:53 +0100

    Recréation des routes supprimées par Breeze

--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -14,6 +14,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('/')->group(function () {
+    Route::get('/pokedex', 'App\Http\controllers\PokemonController@index');
+});
+
+Route::prefix('/decouverteDeLaravel')->group(function () {
+    Route::get('/', function () {
+        return 'Hello world from return';
+    });
+
+    Route::get('/hello', function () {
+        echo 'Hello world from echo';
+    });
+
+    Route::get('/listePokemons', function () {
+        return 'Liste des pokémons';
+    });
+
+    Route::get('/mauvaiseFacon', function () {
+        return
+    '
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mauvaise façon</title>
+    </head>
+    <body>
+        <p>Le fichier risque d\'être longggggg</p>
+    </body>
+    </html>
+    ';
+    });
+
+    Route::get('/listePokemonsView', function () {
+        return view('listePokemons');
+    });
+
+    Route::get('/listePokemonsController/{param?}', 'App\Http\controllers\listePokemonsController@getListePokemons');
+
+    Route::get('/{prenom}/{nom}', function ($prenom, $nom) {
+        return "Hello $prenom $nom";
+    });
+
+    Route::get('/{title}', function ($title) {
+        return "Film : $title";
+    })->where('title', '[a-zA-Z ]+');
+});
+
+// ===== Partie créer par Breeze =========================================
+
 Route::get('/', function () {
     return view('welcome');
 });

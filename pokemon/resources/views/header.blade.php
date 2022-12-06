239684c5198d703d3f776d2a9d44e17c79f95953
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-06 11:24:09 +0100

    Profil dans le header

--- pokemon/resources/views/header.blade.php
+++ pokemon/resources/views/header.blade.php
@@ -1,5 +1,13 @@
 <nav class="navbar navbar-light bg-light fixed-top">
     <a class="navbar-brand" href="{{ route('accueil') }}">Pokémon</a>
+    <div>
+        @auth
+            <a class="btn btn-outline-success" href="{{ route('profile.edit') }}">Profil</a>
+        @else
+            <a class="btn btn-outline-success" href="{{ route('login') }}">Se connecter</a>
+            <a class="btn btn-outline-success" href="{{ route('register') }}">Créer un compte</a>
+        @endauth
+    <div>
 </nav>
 <nav class="navbar navbar-light bg-light">
     <a class="navbar-brand" href="{{ route('accueil') }}">Pokémon2</a>
@@ -9,9 +17,4 @@
     <a class="nav-item nav-link" href="{{ route('pokedex') }}">Pokedex</a>
     <a class="nav-item nav-link" href="{{ route('combat') }}">Combat</a>
     <a class="nav-item nav-link" href="{{ route('historique') }}">Historique</a>
-    @auth
-        <a class="nav-item nav-link" href="{{ route('profile.edit') }}">Profil</a>
-    @else
-        <a class="nav-item nav-link" href="{{ route('login') }}">Connexion</a>
-    @endauth
 </nav>

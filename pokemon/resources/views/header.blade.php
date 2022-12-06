7be58fca6186dc685e0fb59fbe75634883e7abb6
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-06 09:55:17 +0100

    Création de la navbar

--- pokemon/resources/views/header.blade.php
+++ pokemon/resources/views/header.blade.php
@@ -1,1 +1,14 @@
-<p>Mon site Pokémon - header</p>
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="{{ route('accueil') }}">Pokémon</a>
+</nav>
+<nav class="navbar navbar-light bg-light">
+    <a class="nav-link" href="{{ route('accueil') }}">Accueil</a>
+    <a class="nav-link" href="{{ route('pokedex') }}">Pokedex</a>
+    <a class="nav-link" href="{{ route('combat') }}">Combat</a>
+    <a class="nav-link" href="{{ route('historique') }}">Historique</a>
+    @auth
+        <a class="nav-link" href="{{ route('profile.edit') }}">Profil</a>
+    @else
+        <a class="nav-link" href="{{ route('login') }}">Connexion</a>
+    @endauth
+</nav>

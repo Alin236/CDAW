1acad43f812228858d282504f1178c969d869062
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-07 09:22:37 +0100

    css général

--- pokemon/resources/views/template.blade.php
+++ pokemon/resources/views/template.blade.php
@@ -4,13 +4,14 @@
         <title>Pokémon</title> 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/pokemon.css') }}">
         @yield('css')
     </head>
     <body>
         <header>
             @include('header')
         </header>
-        <div>
+        <div id="contenu">
             @yield('content')
         </div>
         <footer>

4f18030ac14e362292f554d134139b04e757e7ad
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-12-09 15:24:34 +0100

    Correction taille des cartes

--- pokemon/resources/views/menuCombat.blade.php
+++ pokemon/resources/views/menuCombat.blade.php
@@ -5,54 +5,54 @@
 @endsection
 
 @section('content')
-<div class="row row-cols-1 row-cols-md-2 g-4">
-    <a class="col" href="">
-        <div class="card h-100">
-            <h3 class="card-header text-center">Combat classique</h3>
-            <div class="card-body">
-                <h4 class="text-center text-warning">Mode de jeu à venir</h4>
-                <h5 class="card-title">Règle</h5>
-                <ul>
-                    <li>3 pokémons vs 3 pokémons</li>
-                    <li>Pokémons choisi</li>
-                    <li>Ordre choisi par le dresseur</li>
-                    <li>Tours jouer par le dresseur</li>
-                    <ul><li>Durée d'un tour : 30s</li><ul>
-                </ul>
-            </div>
+    <div class="card-deck">
+        <div class="card">
+            <a href="">
+                <h3 class="card-header text-center">Combat classique</h3>
+                <div class="card-body">
+                    <h4 class="text-center text-warning">Mode de jeu à venir</h4>
+                    <h5 class="card-title">Règle</h5>
+                    <ul>
+                        <li>3 pokémons vs 3 pokémons</li>
+                        <li>Pokémons choisi</li>
+                        <li>Ordre choisi par le dresseur</li>
+                        <li>Tours jouer par le dresseur</li>
+                        <ul><li>Durée d'un tour : 30s</li><ul>
+                    </ul>
+                </div>
+            </a>
         </div>
-    </a>
-    <a class="col" href="">
-        <div class="card h-100">
-            <h3 class="card-header text-center">Combat semi-automatique</h3>
-            <div class="card-body">
-                <h4 class="text-center text-warning">Mode de jeu à venir</h4>
-                <h5 class="card-title">Règle</h5>
-                <ul>
-                    <li>3 pokémons vs 3 pokémons</li>
-                    <li>Pokémons aléatoire</li>
-                    <li>Ordre choisi par le dresseur</li>
-                    <li>Tours jouer par le dresseur</li>
-                </ul>
-            </div>
+        <div class="card">
+            <a href="">
+                <h3 class="card-header text-center">Combat semi-automatique</h3>
+                <div class="card-body">
+                    <h4 class="text-center text-warning">Mode de jeu à venir</h4>
+                    <h5 class="card-title">Règle</h5>
+                    <ul>
+                        <li>3 pokémons vs 3 pokémons</li>
+                        <li>Pokémons aléatoire</li>
+                        <li>Ordre choisi par le dresseur</li>
+                        <li>Tours jouer par le dresseur</li>
+                    </ul>
+                </div>
+            </a>
         </div>
-    </a>
-    <a class="col" href="">
-        <div class="card h-100">
-            <h3 class="card-header text-center">Combat automatique</h3>
-            <div class="card-body">
-                <h4 class="text-center text-warning">Mode de jeu à venir</h4>
-                <h5 class="card-title">Règle</h5>
-                <ul>
-                    <li>3 pokémons vs 3 pokémons</li>
-                    <li>Pokémons aléatoire</li>
-                    <li>Ordre aléatoire</li>
-                    <li>Tours automatique</li>
-                </ul>
-            </div>
+        <div class="card">
+            <a href="">
+                <h3 class="card-header text-center">Combat automatique</h3>
+                <div class="card-body">
+                    <h4 class="text-center text-warning">Mode de jeu à venir</h4>
+                    <h5 class="card-title">Règle</h5>
+                    <ul>
+                        <li>3 pokémons vs 3 pokémons</li>
+                        <li>Pokémons aléatoire</li>
+                        <li>Ordre aléatoire</li>
+                        <li>Tours automatique</li>
+                    </ul>
+                </div>
+            </a>
         </div>
-    </a>
-</div>
+    </div>
 @endsection
 
 @section('javascript')
@@ -62,7 +62,7 @@
             return false;
         }
 
-        $('a.col').attr('onclick', 'return modeAVenir()');
+        $('.card a').attr('onclick', 'return modeAVenir()');
     </script>
 @endsection
 

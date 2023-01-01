6d65e37a651bf8936dc25dc16457762a123f6b43
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-01 17:52:15 +0100

    function getCombatClassique avec des data test

--- pokemon/app/Http/Controllers/CombatController.php
+++ pokemon/app/Http/Controllers/CombatController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{User, Pokemon};
 
 class CombatController extends Controller
 {
     public function menu()
     {
         return view('menuCombat');
+    }
+
+    public function getCombatClassique(){
+        $joueurs = User::whereIn('id', [1,2])->get();
+        $pokemons = collect([Pokemon::whereIn('id', [1,2,3])->get()->toArray(), Pokemon::whereIn('id', [4,5,6])->get()->toArray()]);
+        return view('combatClassique', Compact('joueurs', 'pokemons'));
     }
 }

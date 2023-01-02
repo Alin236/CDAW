c8038ae965542636a265c2d7cfdb92dfe990e422
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-02 14:32:22 +0100

    Combat classique (non fini)

--- pokemon/resources/views/combatClassique.blade.php
+++ pokemon/resources/views/combatClassique.blade.php
@@ -5,6 +5,11 @@
 
 @section('construction')
     <p>Le combat classique en tour par tour</p>
+    <button onclick="jouer(Action.attaque)">attaque</button>
+    <button onclick="jouer(Action.attaqueSpe)">attaque spé</button>
+    <br>
+    <button onclick="jouer(Action.defenseSpe)">defense spé</button>
+    <button onclick="jouer(Action.fuite)">fuite</button>
 @endsection
 
 @section('javascript')
@@ -14,6 +19,7 @@
         let pokemonActuelIndex = [1,2];
     </script>
     <script type="text/javascript" src="{{ asset('js/systemeCombatGeneral.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/systemeCombatClassique.js') }}"></script>
     
 @endsection
 

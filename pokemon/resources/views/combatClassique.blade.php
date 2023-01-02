5867b81fb9d4aca98d0f3610bd55e6d9f7de74b0
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-02 16:09:49 +0100

    Interface de combat classique

--- pokemon/resources/views/combatClassique.blade.php
+++ pokemon/resources/views/combatClassique.blade.php
@@ -1,15 +1,130 @@
-@extends('pageEnConstruction')
+@extends('template')
 
 @section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/combat.css') }}"/>
 @endsection
 
-@section('construction')
-    <p>Le combat classique en tour par tour</p>
-    <button onclick="jouer(Action.attaque)">attaque</button>
-    <button onclick="jouer(Action.attaqueSpe)">attaque spé</button>
-    <br>
-    <button onclick="jouer(Action.defenseSpe)">defense spé</button>
-    <button onclick="jouer(Action.fuite)">fuite</button>
+@section('content')
+    <div class="card-deck">
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="commandeBox">
+        <div class="commande">
+            <button onclick="jouer(Action.attaque)">attaque</button>
+            <button onclick="jouer(Action.attaqueSpe)">attaque spé</button>
+        </div>
+        <div id="description">
+            <p></p>
+        </div>
+        <div class="commande">
+            <button onclick="jouer(Action.defenseSpe)">defense spé</button>
+            <button onclick="jouer(Action.fuite)">fuite</button>
+        </div>
+    </div>
+    <div class="card-deck">
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card">
+            <h5 class="card-header text-center"></h5>
+            <div class="card-body">
+                <ul>
+                    <li>PV : <span></span></li>
+                    <li>Defense : <span></span></li>
+                </ul>
+                <hr class="hr" />
+                <img src="">
+                <hr class="hr" />
+                <ul>
+                    <li>Attaque : <span></span></li>
+                    <li>Attaque spé : <span></span></li>
+                    <li>Defense spé : <span></span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
 @endsection
 
 @section('javascript')

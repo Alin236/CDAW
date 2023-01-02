bd2afd3e9a160151e7e16cbe53f8a90301d2e092
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-02 23:13:51 +0100

    Battle type migration et seeder

---
+++ pokemon/database/seeders/BattleTypeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BattleTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('battle_type')->insert(['name' => 'Classique']);
+        DB::table('battle_type')->insert(['name' => 'Semi-automatique']);
+        DB::table('battle_type')->insert(['name' => 'Automatique']);
+    }
+}

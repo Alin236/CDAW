c3be7e37461e992ecb919c1fbef8696c4c525d38
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2023-01-19 20:00:27 +0100

    Les battles sont ordonnées du plus récent au plus vieux dans l'historique

--- pokemon/app/Http/Controllers/HistoriqueController.php
+++ pokemon/app/Http/Controllers/HistoriqueController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 class HistoriqueController extends Controller
 {
     function index(){
-        $battles = Battle::where('id_joueur_1', Auth::user()->id)->orWhere('id_joueur_2', Auth::user()->id)->get();
+        $battles = Battle::where('id_joueur_1', Auth::user()->id)->orWhere('id_joueur_2', Auth::user()->id)->orderBy('created_at', 'desc')->get();
         return view('historique', compact('battles'));
     }
 }
